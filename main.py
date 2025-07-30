02cbeee4065387e5e7fae49ec4bb943af0752fdf
greybubble <greybubble@gmail.com>
2025-07-30 17:27:21 -0500

    added sorting function: sort by most to least frequent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_symbol_frequency
+from stats import count_words, count_symbol_frequency, create_sorted_list
 
 def get_book_text(path_to_book):
     with open(path_to_book) as text:
@@ -7,13 +7,17 @@ def get_book_text(path_to_book):
 
 def main():
 
-    frankenstein = get_book_text("books/frankenstein.txt")
+    path_to_frank = "books/frankenstein.txt"
+
+    frankenstein = get_book_text(path_to_frank)
     print(count_words(frankenstein), "words found in the document")
     
     symbol_count = count_symbol_frequency(frankenstein)
 
-    symbol_count = dict(sorted(symbol_count.items()))
+    # print(symbol_count)
 
-    print(symbol_count)
+    sorted_list = create_sorted_list(symbol_count)
+
+    print(sorted_list)
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,20 @@ def count_symbol_frequency(text):
             frequency[symbol] = 1
 
     return frequency
+
+def sort_on(items):
+    return items["num"]
+
+def create_sorted_list(dict):
+    sorted_list = []
+    for entry in dict:
+        if(entry.isalpha()):
+            char_count = {}
+            char_count["char"] = entry
+            char_count["num"] = dict[entry]
+        
+            sorted_list.append(char_count)
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+        
